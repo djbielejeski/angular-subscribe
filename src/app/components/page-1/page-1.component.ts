da2d9546b14dea8adcd563771ed3ecca07495fc7
djbielejeski <djbielejeski@gmail.com>
2018-01-16 08:52:11 -0600

    DB:  Showing the bad way of unsubscribing and adding comments.

--- src/app/components/page-1/page-1.component.ts
+++ src/app/components/page-1/page-1.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/operator/takeUntil';
 
 import { BaseComponent } from '@app/components/core';
@@ -10,7 +11,9 @@ import { ServerDataModel } from '@app/models';
     selector: 'app-page-1',
     templateUrl: './page-1.component.html'
 })
-export class Page1Component extends BaseComponent implements OnInit {
+export class Page1Component extends BaseComponent implements OnInit, OnDestroy {
+    // Duplicated code every time you have an infinite subscription.
+    private badWayToUnsubscribe_Subscription = new Subscription();
 
     constructor(private router: Router,
                 private httpHelperService: HttpHelperService) {
@@ -18,21 +21,37 @@ export class Page1Component extends BaseComponent implements OnInit {
     }
 
     ngOnInit() {
-        // Infinite without unsubscribe
+        // Infinite subscription without unsubscribe
         this.router.events.subscribe((event) => {
             if (event instanceof NavigationStart) {
                 console.log('Page 1: Router.Events.Subscribe (Infinite) - No Unsubscribe: ', event);
             }
         });
 
+        // The clean way of unsubscribing from an infinite subscription
         this.router.events.takeUntil(this.ngUnsubscribe).subscribe((event) => {
             if (event instanceof NavigationStart) {
                 console.log('Page 1: Router.Events.Subscribe (Infinite) - With Unsubscribe: ', event);
             }
         });
+
+        // The duplicated code way of unsubscribing from an infinite subscription
+        this.badWayToUnsubscribe_Subscription = this.router.events.subscribe((event) => {
+            if (event instanceof NavigationStart) {
+                console.log('Page 1: Router.Events.Subscribe (Infinite) - With Duplicated Unsubscribe: ', event);
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        super.ngOnDestroy();
+
+        // Duplicated code every time you have an infinite subscription.
+        this.badWayToUnsubscribe_Subscription.unsubscribe();
     }
 
     buttonClick() {
+        // Notice we don't have to unsubscribe here since this is a Finite(1) call thanks to how angular HttpClient works.
         this.httpHelperService.get().subscribe((data: ServerDataModel) => {
            console.log('Page 1: HttpHelperService.get() (Finite): ', data);
         });
